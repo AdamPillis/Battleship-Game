969a70d16842cb8eb611e2ae8bbf06a9af26952a
AdamPillis <adam.pillis@hotmail.com>
2021-11-09 10:56:10 +0000

    Initial commit + start creating board class in run.py

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+
+class main_board:
+    """
+    Will create player and computer board based on
+    user input.
+    """
+    def __init__(self, name, size, ship_nums, type):
+        self.name = name
+        self.size = size
+        self.ship_nums = ship_nums
+        self.type = type
+        self.board = [['~' for width in range(size)] for height in range(size)]
+        self.ships = []
+        self.guesses = []
+
+    def print_board(self):
+        for row in board:
+            print(' '.join(row))
+
