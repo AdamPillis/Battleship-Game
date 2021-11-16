383c0ef7b9877a6977876d8a23710fded3241be6
AdamPillis <adam.pillis@hotmail.com>
2021-11-16 16:25:27 +0000

    Add: scores function to display scores at end of each round

--- run.py
+++ run.py
@@ -52,17 +52,20 @@ def get_user_data():
     while True:
         print('Numbers must be between 5 and 10\n')
         size = input('Size of your square board:\n')
+        print('Validating input...\n')
+        time.sleep(1)
 
         new_size = validate_user_data(size, 5, 10)
 
         if new_size:
-        
+
             ships = input('Number of ships on each board:\n')
+            print('Validating input...\n')
+            time.sleep(1)
 
             new_ships = validate_user_data(ships, 5, 10)
 
             if new_ships:
-                print('Creating board...\n')
                 break
 
     return [size, ships]
@@ -112,18 +115,17 @@ def populate_game_board(board):
 def make_guess(board):
     """
     For computer, generates random x and y coordanites.
-    For player, row and col input is requested. 
+    For player, row and col input is requested.
     """
     size = board.size
     while True:
         if board.type == 'computer':
-            print("Computer's turn to guess")
             row_guess = random_number(board.size)
             col_guess = random_number(board.size)
         else:
             row_guess = input('Enter row num:\n')
             col_guess = input('Enter column num:\n')
-            
+
         row = validate_user_data(str(row_guess), 0, (size - 1))
         col = validate_user_data(str(col_guess), 0, (size - 1))
 
@@ -146,12 +148,18 @@ def validate_guess(board, other_board, x, y):
     if (x, y) in other_board.ships:
         board.guesses.append((x, y))
         other_board.board[x][y] = '| O'
-        print('A Battleship has been hit!')
+        print('~' * 60)
+        print(f"{board.name}'s coordinates {(x, y)}")
+        time.sleep(1)
+        print('A Battleship has been hit!\n')
         return True
     else:
         board.guesses.append((x, y))
         other_board.board[x][y] = '| X'
-        print('Missile missed target...')
+        print('~' * 60)
+        print(f"{board.name}'s coordinates {(x, y)}")
+        time.sleep(1)
+        print('Missed target...\n')
         return True
 
 
@@ -160,7 +168,7 @@ def play_game(board, other_board, ships):
     """
     while True:
         populate_game_board(board)
-        print('~' * 60)
+        print('~' * 50)
         populate_game_board(other_board)
 
         player_guess = make_guess(board)
@@ -169,15 +177,28 @@ def play_game(board, other_board, ships):
         valid = validate_guess(board, other_board, p_row, p_col)
         if valid is False:
             return play_game(board, other_board, ships)
-            
+
         comp_guess = make_guess(other_board)
         c_row = comp_guess[0]
         c_col = comp_guess[1]
-        validate_guess(other_board, board, c_row, c_col)
+        valid_two = validate_guess(other_board, board, c_row, c_col)
+        scores(board, valid)
+        scores(other_board, valid_two)
 
         if len(other_board.guesses) == ships:
             break
     print('finished play game')
+
+
+def scores(board, hit):
+    """
+    """
+    board_score = 0
+
+    if hit:
+        board_score += 5
+        print('~' * 60)
+        print(f"{board.name}'s score is now: {board_score}")
 
 
 def new_game():
@@ -185,27 +206,28 @@ def new_game():
     Runs main game every time user reloads
     or restarts the game
     """
-    print('~' * 50)
+    print('~' * 60)
     print('WELCOME TO BATTLESHIPS')
-    print('~' * 50)
+    print('~' * 60)
     name = input('Please enter your name here:\n')
     time.sleep(0.5)
     print(f"Hi {name}. Let's go through some rules first...\n")
     time.sleep(1)
-    print('~' * 50)
+    print('~' * 60)
     print('1. Number of rounds will be equal to the number of ships chosen.\n')
     print('2. Each ship sank is worth 5 points\n')
     print('3. Top left hand corner is row 0, col 0\n')
     print('4. Number of rows will be equal to the number of columns')
-    print('~' * 50)
+    print('~' * 60)
     # time.sleep(5)
     input('Press Enter to start your game.\n')
-    print('Creating new game...\n')
-    time.sleep(1.5)
-
     data = get_user_data()
     size = int(data[0])
     num_of_ships = int(data[1])
+    print(f'Remember, rows and columns will now range from 0 to {size - 1}\n')
+    time.sleep(2)
+    print('Creating new game...\n')
+    time.sleep(2)
 
     player_board = Board(name, size, num_of_ships, type='player')
     computer_board = Board('Computer', size, num_of_ships, type='computer')
@@ -213,7 +235,7 @@ def new_game():
     play_game(player_board, computer_board, num_of_ships)
 
     populate_game_board(player_board)
-    print('~' * 50)
+    print('~' * 60)
     populate_game_board(computer_board)
     print('game over')
 
